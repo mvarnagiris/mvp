0672655cfba6dc8b6dc8e7bb4c18b12f3689162d
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2018-03-05 21:07:41 +0000

    ErrorView shows error on main scheduler.

--- src/main/kotlin/com/mvcoding/mvp/ErrorView.kt
+++ src/main/kotlin/com/mvcoding/mvp/ErrorView.kt
@@ -1,11 +1,10 @@
 package com.mvcoding.mvp
 
-import io.reactivex.Observable
-
 interface ErrorView<in ERROR> : Presenter.View {
     fun showError(error: ERROR)
 }
 
-fun <T, ERROR, VIEW : ErrorView<ERROR>> O<T>.showErrorAndComplete(view: VIEW, mapError: (Throwable) -> ERROR): O<T> =
-        doOnError { view.showError(mapError(it)) }
-                .onErrorResumeNext(Observable.empty<T>())
+fun <T, ERROR, VIEW : ErrorView<ERROR>> O<T>.showErrorAndComplete(view: VIEW, mapError: (Throwable) -> ERROR, schedulers: RxSchedulers = trampolines): O<T> =
+        onErrorResumeNext { throwable: Throwable ->
+            O.just(throwable).observeOn(schedulers.main).doOnNext { view.showError(mapError(it)) }.flatMap { (O.empty<T>()) }
+        }
