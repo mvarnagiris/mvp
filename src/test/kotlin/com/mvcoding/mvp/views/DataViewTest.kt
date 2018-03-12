b12b64676315a1cd09acb566a3dbd6be96e86441
memoizr <memoizrlabs@gmail.com>
2018-03-12 18:22:59 +0000

    add thread checks

--- src/test/kotlin/com/mvcoding/mvp/views/DataViewTest.kt
+++ src/test/kotlin/com/mvcoding/mvp/views/DataViewTest.kt
@@ -35,27 +35,28 @@ val mainScheduler = Thread.currentThread().name
 val testSchedulers = trampolines.copy(
         io = RxJavaPlugins.createSingleScheduler { Thread(it, "test io") },
         computation = RxJavaPlugins.createSingleScheduler { Thread(it, "test computation") },
-        main = RxJavaPlugins.createSingleScheduler { Thread(it, mainScheduler) }
+        main = RxJavaPlugins.createSingleScheduler { Thread(it, "test main") }
 )
 
 class DataTestAssertion {
 
-    val dataSource = mock<DataSource<String>>()
+    val dataSource = threadMock<DataSource<List<String>>>()
 
-    val presenter = PresenterForTest(dataSource, testSchedulers)
-    val assertion by LoadDataBehaviourAssertion(presenter, dataSource)
+    val presenter = PresenterForTest(dataSource(), testSchedulers)
+    val assertion by LoadDataBehaviourAssertion(presenter, dataSource.proxy)
 
     @Test
     fun `presenter has loadData behavior`() {
         assertion.verifyLoadDataBehavior()
-                .verifyAllViewInteractionOnScheduler(testSchedulers.main)
     }
 
 
-    class PresenterForTest(val dataSource: DataSource<String>, val testSchedulers: RxSchedulers) : Presenter<DataView<String>>() {
-        override fun onViewAttached(view: DataView<String>) {
+    class PresenterForTest(val dataSource: DataSource<List<String>>, val testSchedulers: RxSchedulers) : Presenter<DataView<List<String>>>() {
+        override fun onViewAttached(view: DataView<List<String>>) {
             super.onViewAttached(view)
-            Observable.just("").loadData(view, { dataSource.data() }, testSchedulers).subscribe()
+            Observable.just("").loadData(view, {
+                dataSource.data()
+            }, testSchedulers).subscribe()
         }
     }
 }
@@ -66,54 +67,72 @@ class LoadDataBehaviourAssertion<T, V : DataView<T>>(val presenter: Presenter<V>
         return BehAssertion(presenter, dataSource, dataType)
     }
 
-    class BehAssertion<T, V : DataView<T>>(val presenter: Presenter<V>, val dataSource: DataSource<T>, val dataType: KType) {
+    class BehAssertion<T, V : DataView<T>>(val presenter: Presenter<V>, val dataSource: DataSource<T>, dataType: KType) {
         private val value = instantiateRandomClass(dataType) as T
 
-
-        fun verifyLoadDataBehavior(): ThreadMock<DataView<T>> {
-            val mock = threadMock<DataView<T>>()
+        fun verifyLoadDataBehavior() {
+            val viewMock = threadMock<DataView<T>>()
 
             whenever(dataSource.data()).thenReturn(O.just(value))
 
-            presenter attach mock() as V
+            presenter attach viewMock() as V
 
-            verify(mock).showData(value)
+            verify(viewMock).showData(value)
 
-            mock.verifyAllViewInteractionOnScheduler(testSchedulers.main)
-
-            return mock
+            viewMock.verifyAllInteractionsOn(testSchedulers.main)
+            dataSource.mockWrapper().threadMock().verifyAllInteractionsOn(testSchedulers.io)
         }
     }
 }
 
-inline fun <reified T: Any> verify(mock: ThreadMock<T>): T = Mockito.verify(mock.mock, timeout(100))!!
+inline fun <reified T : Any> verify(mock: ThreadMock<T>): T = Mockito.verify(mock.mock, timeout(100))!!
 
+inline fun <reified T : Any> T.mockWrapper(): MockWrapper {
+    return this as MockWrapper
+}
 
-inline fun <reified T: Any> threadMock(): ThreadMock<T> {
+inline fun <reified T : Any> threadMock(): ThreadMock<T> {
     val mock = mock<T>()
     return ThreadMock(mock, T::class)
 }
 
-data class ThreadMock<T: Any>(val mock: T, val mockClass: KClass<T>) {
+data class ThreadMock<T : Any>(val mock: T, val mockClass: KClass<T>) {
 
     private data class ThreadInvocations(val method: String, val threadName: String)
-
     operator fun invoke() = proxy
-
     private val threads = mutableListOf<ThreadInvocations>()
 
-    private val proxy = Proxy.newProxyInstance(mockClass.java.classLoader, arrayOf(mockClass.java),
+    val proxy = Proxy.newProxyInstance(mockClass.java.classLoader, arrayOf(mockClass.java, MockWrapper::class.java),
             { proxy, method, args ->
                 val signature = "${method.name}(${method.parameters.toList().joinToString(",")})"
-                threads += ThreadInvocations(signature, Thread.currentThread().name)
-                mock::class.java.methods.find { it.name == method.name }?.invoke(mock, *args)
+
+                if (method.name == MockWrapper::threadMock.name) {
+                    this
+                } else {
+                    val find = mock::class.java.methods.find { it.name == method.name }
+                    if (args == null) {
+                        val invoke = find?.invoke(mock)
+
+                        if (find != null && find?.returnType.name == "void" || invoke != null) {
+                            threads += ThreadInvocations(signature, Thread.currentThread().name)
+                        }
+
+                        invoke
+                    } else {
+                        val invoke = find?.invoke(mock, *args)
+                        if (find != null && find?.returnType.name == "void" || invoke != null) {
+                            threads += ThreadInvocations(signature, Thread.currentThread().name)
+                        }
+                        invoke
+                    }
+                }
             }) as T
 
-    fun verifyAllViewInteractionOnScheduler(scheduler: Scheduler) {
+    fun verifyAllInteractionsOn(scheduler: Scheduler) {
         val latch = CountDownLatch(1)
 
         var t: Throwable? = null
-        scheduler.scheduleDirect{
+        scheduler.scheduleDirect {
             try {
                 expect that threads.all { it.threadName == Thread.currentThread().name } describedAs "Expected all invocations to occur on ${Thread.currentThread().name}, but some invocations occurred on a different thread :${threads}" _is true
             } catch (e: Throwable) {
@@ -126,4 +145,8 @@ data class ThreadMock<T: Any>(val mock: T, val mockClass: KClass<T>) {
         latch.await()
         t?.run { throw this }
     }
+}
+
+interface MockWrapper {
+    fun threadMock(): ThreadMock<*>
 }
